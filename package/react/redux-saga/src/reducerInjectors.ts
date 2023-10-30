18ea857a05e4e63b07363867a9fbe4faaabeceae
nodepro777@gmail.com <nodepro777@gmail.com>
2023-10-30 15:55:11 +0100

    removed space

--- package/react/redux-saga/src/reducerInjectors.ts
+++ package/react/redux-saga/src/reducerInjectors.ts
@@ -1,10 +1,10 @@
 import {Store} from 'redux';
 import {createReducer} from "./index";
+
 export function injectReducerFactory (store:Store<any, any>) {
   return function injectReducer(key:string, reducer:Function) {
       //@ts-ignore
-	  if (Reflect.has(store.injectedReducers, key) && store.injectedReducers[key] === reducer)
-      return;
+	  if (Reflect.has(store.injectedReducers, key) && store.injectedReducers[key] === reducer)  return;
       //@ts-ignore
 	  store.injectedReducers[key] = reducer
       //@ts-ignore
