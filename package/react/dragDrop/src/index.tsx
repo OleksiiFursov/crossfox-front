0a4a4fc477b57041a704ab51b7e6c0bc82a158be
nodepro777@gmail.com <nodepro777@gmail.com>
2023-11-02 14:52:20 +0100

    start dragDrop

---
+++ package/react/dragDrop/src/index.tsx
@@ -0,0 +1,300 @@
+import React, {useEffect, useState, createElement, DragEventHandler, MouseEventHandler} from 'react';
+import {IDragDropContext, iDragDropElement, iDragDropZone, iSelection} from "./types";
+
+const d = document;
+const dQ = (s: string) => d.querySelector(s);
+
+const initialSelections = () => ({
+	items: [],
+	itemsElement: [],
+	owner: null,
+	dropTarget: null,
+	dropTargetElement: null
+});
+
+let related: HTMLElement;
+let isMouseMoveAvailable = false;
+
+
+function getContainer(el: HTMLElement) {
+	while (el) {
+		if (el.nodeType === 1 && el.getAttribute('aria-dropeffect')) break
+
+		el = el.parentNode as HTMLElement;
+	}
+	return el;
+}
+
+const find = (name: string) => dQ("[name='" + name + "']");
+const getName = (query: HTMLElement) => query && query.getAttribute('name') || '';
+const each = (query: string, callback: Function, parent = d) => {
+	const els = parent.querySelectorAll(query);
+	for (let i = 0; i < els.length; i++) {
+		callback(els[i], i);
+	}
+};
+
+
+const HandleDragEnter: DragEventHandler = e => {
+	related = e.currentTarget as HTMLElement;
+}
+
+export function DragDropContext(props: IDragDropContext) {
+
+	const {onDragEnd, children, selected = []} = {
+		onDragEnd: (selections: iSelection) => {
+			for (let i = 0; i < selections.items.length; i++) {
+				selections.dropTarget.appendChild(selections.items[i]);
+			}
+		}, ...props
+	};
+
+	const [selections, setSelections] = useState(initialSelections());
+
+	useEffect(() => {
+		setSelections(initialSelections());
+	}, []);
+
+	useEffect(() => {
+		for (const id of selected) {
+			if (!selections.items.includes(id)) {
+				addSelection(find(id));
+			}
+		}
+		for (const id of selections.items) {
+			if (!selected.includes(id)) {
+				removeSelection(find(id));
+			}
+		}
+	}, [selected]);
+
+
+	const addSelection = (item: HTMLElement) => {
+		if (!selections.owner && item) {
+			selections.owner = item.parentNode;
+		} else if (selections.owner !== item.parentNode) {
+			return;
+		}
+
+		item.setAttribute('aria-grabbed', 'true');
+
+		selections.items.push(item.getAttribute('name'));
+		selections.itemsElement.push(item);
+		setSelections({...selections});
+	}
+
+
+	//function for unselecting an item
+	const removeSelection = (item: HTMLElement): void => {
+		if (!item) return;
+		item.setAttribute('aria-grabbed', 'false');
+
+		for (let i = 0; i < selections.items.length; i++) {
+			if (selections.items[i] === item) {
+				selections.itemsElement.splice(i, 1);
+				selections.items.splice(i, 1);
+				break;
+			}
+		}
+		setSelections({...selections});
+	}
+
+	const clearSelections = () => {
+		if (selections.items.length) {
+			selections.owner = null;
+
+			for (let i = 0; i < selections.itemsElement.length; i++) {
+				if (!selected.includes(selections.items[i]) && selections.itemsElement[i])
+					selections.itemsElement[i].setAttribute('aria-grabbed', 'false');
+			}
+
+			selections.items = [];
+			selections.itemsElement = Array(selected.length).fill(0).map(find);
+			setSelections({...selections});
+		}
+
+	}
+
+	const hasModifier = e => e.ctrlKey || e.metaKey || e.shiftKey;
+
+
+	function addDropEffects() {
+
+		let ownAttrName = getName(selections.owner);
+
+		if (ownAttrName)
+			ownAttrName = ":not(" + ownAttrName + ")";
+
+		each('[aria-dropeffect="none"]' + ownAttrName,
+			v => v.setAttribute('aria-dropeffect', 'move')
+		);
+		each('[aria-grabbed]' + ownAttrName,
+			v => v.removeAttribute('aria-grabbed')
+		);
+	}
+
+	function clearDropEffects() {
+
+		if (selections.items.length) {
+			each('[data-draggable="target"]:not([aria-dropeffect="none"])',
+				v => v.setAttribute('aria-dropeffect', 'none')
+			);
+
+			each('[data-draggable="item"]:not([aria-grabbed])',
+				v => v.setAttribute('aria-grabbed', 'false')
+			);
+
+		}
+	}
+
+
+	const HandleMouseDown:MouseEventHandler = (e:MouseEvent) => {
+		const el = (e.target.closest('[data-draggable="item"]') || e.target) as HTMLElement;
+		if (el.getAttribute('draggable')) {
+			clearDropEffects();
+
+			if (!hasModifier(e) && el.getAttribute('aria-grabbed') === 'false') {
+				clearSelections();
+				addSelection(el);
+			}
+		} else if (!hasModifier(e)) {
+			clearDropEffects();
+			clearSelections();
+		} else {
+			clearDropEffects();
+		}
+	}
+
+	const HandleMouseUp:MouseEventHandler = e => {
+		const el = e.target.closest('[data-draggable="item"]') || e.target;
+		if (el.getAttribute('draggable') && hasModifier(e)) {
+
+			if (el.getAttribute('aria-grabbed') === 'true') {
+				removeSelection(el);
+
+				if (!selections.items.length) {
+					selections.owner = null;
+					setSelections({...selections});
+				}
+			} else {
+
+				addSelection(el);
+			}
+		}
+	};
+
+	const HandleDragStart = e => {
+
+		if (selections.owner !== e.target.parentNode) { //closest
+			e.preventDefault();
+			return;
+		}
+		const a = e.target.parentElement;
+		if (hasModifier(e) && a.getAttribute('aria-grabbed') === 'false') {
+			addSelection(a);
+		}
+
+		e.dataTransfer.setData('text', '');
+
+		addDropEffects();
+	}
+
+
+	const HandleDragLeave: DragEventHandler = () => {
+
+		let dropTargetElement = getContainer(related);
+
+		if (dropTargetElement === selections.owner) {
+			dropTargetElement = null;
+		}
+
+		if (dropTargetElement !== selections.dropTargetElement) {
+			if (selections.dropTargetElement) {
+				(selections.dropTargetElement as HTMLElement).classList.remove('dragover');
+			}
+			if (dropTargetElement) {
+				dropTargetElement.classList.add('dragover');
+			}
+
+			selections.dropTargetElement = dropTargetElement;
+			selections.dropTarget = getName(dropTargetElement as HTMLElement);
+			setSelections({...selections});
+		}
+	}
+
+	const HandleDragOver = e => {
+		if (isMouseMoveAvailable) {
+			setTimeout(() => {
+				isMouseMoveAvailable = true;
+			}, 30);
+		}
+		isMouseMoveAvailable = false;
+
+		if (selections.items.length) e.preventDefault();
+	}
+
+	const HandleDragEnd: DragEventHandler = e => {
+		if (selections.dropTargetElement) {
+			onDragEnd({...selections})
+			e.preventDefault();
+		}
+
+		if (selections.items.length) {
+			clearDropEffects();
+
+			if (selections.dropTargetElement) {
+				clearSelections();
+
+				selections.dropTargetElement.classList.remove('dragover')
+
+				selections.dropTargetElement = null;
+				selections.dropTarget = null;
+				setSelections({...selections});
+			}
+		}
+
+	};
+
+	return <div onDragEnd={HandleDragEnd}
+							onDragOver={HandleDragOver}
+							onDragLeave={HandleDragLeave}
+							onDragEnter={HandleDragEnter}
+							onDragStart={HandleDragStart}
+							onMouseUp={HandleMouseUp}
+							onMouseDown={HandleMouseDown}
+	>
+		{children}
+	</div>;
+}
+
+
+export function DragDropZone(props: iDragDropZone) {
+	const {children, tagName = 'div', component: Component, ...attrs} = props;
+
+	const properties = {
+		'data-draggable': "target",
+		'aria-dropeffect': "none",
+		...attrs
+	};
+
+	if (Component)
+		return <Component {...properties}>{children}</Component>;
+
+	return createElement(tagName, properties, children);
+}
+
+export function DragDropElement(props: iDragDropElement) {
+	const {tagName = 'div', children, draggable = true, component: Component, ...attrs} = props;
+
+	const properties = {
+		'data-draggable': draggable ? "item" : 'item-no-drop',
+		'aria-grabbed': false,
+		draggable,
+		...attrs
+	};
+
+	if (Component)
+		return <Component {...properties}>{children}</Component>;
+
+	return createElement(tagName, properties, children);
+}
---
+++ package/react/dragDrop/src/types.ts
@@ -0,0 +1,53 @@
+import {ReactNode, JSX, FC} from "react";
+
+export interface IDragDropContext {
+	children: ReactNode,
+	onDragEnd?: Function,
+	selected?: string[]
+}
+
+export interface iDragDropElement {
+	children: ReactNode
+	tagName?: keyof JSX.IntrinsicElements,
+	draggable?: boolean
+	component: FC
+}
+
+export interface iSelection {
+	items: HTMLElement[],
+	itemsElement: HTMLElement[],
+	owner: HTMLElement | ParentNode,
+	dropTarget: HTMLElement,
+	dropTargetElement: HTMLElement
+}
+
+
+export interface iDragDropZone{
+	children: ReactNode,
+	tagName?: keyof JSX.IntrinsicElements,
+	component: FC
+}
+//
+// export interface iRouterContext{
+// 	baseUrl: string,
+// 	onChange: (prev:string, current:string) => void,
+// 	sUrl: string,
+// 	setSUrl: (url: string, data: Record<string, any>) => void,
+// 	sHistory: MutableRefObject<[string, Record<string, any>][] | null>,
+// 	sLayout: MutableRefObject<string | null>
+// }
+//
+// export interface iRouterLink{
+// 	to: string,
+// 	tagName?: string,
+// 	children?: FC,
+// 	data?: Record<string, string>,
+// 	onClick?: (e:MouseEvent)=>void,
+// 	href?: string
+// }
+//
+// export interface iRouteConfig{
+// 	layout: string,
+// }
+//
+//
